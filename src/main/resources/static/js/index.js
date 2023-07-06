7612de1df30299aaf310e066f85b35e607352917
Marie5646 <125408068+Marie5646@users.noreply.github.com>
2023-07-06 16:52:22 -0500

    changes to index

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,16 +4,14 @@ function upcomingEvents(){
     fetch(`http://localhost:8080/events/closestEvents`)
         .then( response => { response.json()
             .then(events => {
-                for (let i = 0; i < events.length; i++) {
+                for (let i = 0; i >= 0; i++) {
                     let html = "";
-                    console.log(events[i].title)
                     html += `<div class="col-lg-3">`
                     html += `<div class="card">`
-                    // html += `<img src="card1.jpg" class="card-img-top" alt="Card 1">`
                     html += `<div class="card-body">`
                     html += `<h5 class="card-title">${events[i].title}</h5>`
                     html += `<p class="card-text">${events[i].description}</p>`
-                    html += `<a href="#" class="btn btn-light">Read More</a>`
+                    html += `<a href="/event/${events[i].id}/reviews" class="btn btn-light">Read More</a>`
                     html += `</div>`
                     html += `</div>`
                     html += `</div>`
